5df8f7a76a068a25e0761a387f96b0c473305c6a
mpone <m.parphenjuk@gmail.com>
2020-09-07 18:25:20 +0300

    Updates

---
+++ precache-manifest.2ce0db5c882c467a9fd54122abb11535.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00d62b3cced92876f2b7d6505a642472",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9f9eb9e353ac52ed2c10",
+    "url": "./static/css/2.67a0cd86.chunk.css"
+  },
+  {
+    "revision": "e3613ae8dbae556eb4af",
+    "url": "./static/css/main.ed159b4a.chunk.css"
+  },
+  {
+    "revision": "9f9eb9e353ac52ed2c10",
+    "url": "./static/js/2.0a2c8cb1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.0a2c8cb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3613ae8dbae556eb4af",
+    "url": "./static/js/main.251c9aaf.chunk.js"
+  },
+  {
+    "revision": "3cfb45b61c015a055433",
+    "url": "./static/js/runtime-main.0527da25.js"
+  }
+]);
